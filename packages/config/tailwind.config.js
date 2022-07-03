fa61863d6f8822e87da001398b0a43404a01c20c
Alfredo Perez <alfredo.perez.q@gmail.com>
2022-07-03 13:33:14 -0500

    eslint

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
-    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    '../../packages/ui/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
