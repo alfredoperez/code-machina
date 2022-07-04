cb7d7e0412cbde64278f25964c29eced3c5ab4a0
Alfredo Perez <alfredo.perez.q@gmail.com>
2022-07-04 15:08:41 -0500

    Adds tailwind support to StoryBook

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,9 +1,6 @@
+import '../styles/tailwind.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  layout: 'centered',
+};
