b1fbfeb72cb4ea4785543b7b329ec8660be7d35e
Alfredo Perez <alfredo.perez.q@gmail.com>
2023-09-30 17:22:37 -0500

    adds syntax and loads list of blog posts

---
+++ data/siteMetadata.ts
@@ -0,0 +1,43 @@
+const siteMetadata = {
+  description: 'A starter template to build amazing static websites with Next.js and Contentlayer',
+  locale: 'en',
+  language: 'en',
+  siteUrl: 'https://nextjs-contentlayer-starter.vercel.app',
+  title: 'Next.js Contentlayer Starter',
+  socialBanner: '/social.png',
+  // Features
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    umamiAnalytics: {
+      // We use an env variable for this site to avoid other users cloning our analytics ID
+      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+    },
+    // plausibleAnalytics: {
+    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    // },
+    // simpleAnalytics: {},
+    // posthogAnalytics: {
+    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
+    // googleAnalytics: {
+    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
+    // },
+  },
+  search: {
+    provider: 'kbar', // kbar or algolia
+    kbarConfig: {
+      searchDocumentsPath: 'search.json', // path to load documents to search
+    },
+    // provider: 'algolia',
+    // algoliaConfig: {
+    //   // The application ID provided by Algolia
+    //   appId: 'R2IYF7ETH7',
+    //   // Public API key: it is safe to commit it
+    //   apiKey: '599cec31baffa4868cae4e79f180729b',
+    //   indexName: 'docsearch',
+    // },
+  },
+}
+export default siteMetadata
