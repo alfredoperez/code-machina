10343a436adcff86ac6f40c852a2db6bb6b98b55
Alfredo Perez <alfredo.perez.q@gmail.com>
2023-10-14 07:32:56 -0500

    Removes about page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/doc', title: 'Docs' },
   { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
